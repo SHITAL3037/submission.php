6173941ae92349d4f78c9cefe6dc2c0a30ca3a7b
SHITAL3037 <90553149+SHITAL3037@users.noreply.github.com>
2021-09-12 16:14:52 +0530

    Create submission.php

---
+++ submission.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>ANIMAL INFORMATIOM</title>
+</head>
+<body>
+<form align="center">
+<h1>ANIMAL INFORMAYION</h1>
+<table border=1 align="center">
+<tr>
+<td>Enter Name of Animal:</td>
+<td><input type="text" name="txtnm" id="txtnm"/></td>
+</tr><tr>
+<td>Category of Animal:</td>
+<td><input type="radio" name="herbi" id="herbi"/>herbivores<br/>
+<input type="radio" name="amni" id="amni"/>anmivores<br/>
+<input type="radio" name="carni" id="carni"/>carnivores</td>
+</tr>
+<tr>
+<td>Image:</td>
+<td><img scr="" height="300px" width="300px"/></td>
+</tr>
+<tr>
+<td>Animal Description:</td>
+<td><textarea name="txtdesc" id="txtdesc"></textarea></td>
+</tr>
+<tr>
+<td>Life Expectancy:</td>
+<td><input type="checkbox" name="check1" id="check1"/>0-1year<br>
+<input type="checkbox" name="check2" id="check2"/>1-5year<br>
+<input type="checkbox" name="check3" id="check3"/>5-10year<br>
+<input type="checkbox" name="check3" id="check3"/>10+year</td>
+</tr>
+<tr>
+<td>Captcha</td>
+<td><input type="text" name="txtcap" id="txtcap"/></td>
+</tr>
+<tr>
+<td colspan="2">
+<div align="center">
+<input type="submit" name="submit" value="Submit"/></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
